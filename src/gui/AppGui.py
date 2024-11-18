14a72466395e9a38c3ff86d002125da8ed34cde1
Lautaro-io <ildarrazlautaro@gmail.com>
2024-11-17 21:22:19 -0300

    last

--- src/gui/AppGui.py
+++ src/gui/AppGui.py
@@ -4,6 +4,7 @@ import customtkinter as ctk
 import qrcode
 from PIL import Image,ImageTk
 import io
+from tkinter import messagebox
 
 from qrcode.console_scripts import commas
 
@@ -14,6 +15,7 @@ class App(ctk.CTk):
 
     def __init__(self):
         super().__init__()
+        self.title("QR GENERATOR.")
         self.center_window()
         self.main()
 
@@ -21,15 +23,11 @@ class App(ctk.CTk):
     def main(self):
         self.grid_columnconfigure(1,weight=1)
         title = ctk.CTkLabel(self,text="Generador de QR" ,font=("Roboto", 25,"bold"))
-        title.grid(row =0 , column = 1 ,pady =20 )
+        title.grid(row =1 , column = 1 ,pady =20 )
         self.entry = ctk.CTkEntry(self,placeholder_text="Ingrese una URL o texto" , width= 250)
-        self.entry.grid(row = 1, column = 1 ,pady = 20)
+        self.entry.grid(row = 2, column = 1 ,pady = 20)
         btn = ctk.CTkButton(self, text="Submit" , width=250, command = self.generate_qr,fg_color="white", text_color="black" , hover_color="gray")
-        btn.grid(row = 2 ,column = 1 )
-
-
-
-
+        btn.grid(row = 3 ,column = 1 )
 
     def center_window(self):
         ancho = 400
@@ -42,20 +40,18 @@ class App(ctk.CTk):
 
         self.geometry(f"{ancho}x{alto}+{x}+{y}")
 
-    def clean_label(self):
-        #self.qr_label.configure(text=" " , image =None)
-        self.qrFrame.destroy()
-        self.cleanBtn.destroy()
-        self.qr_label.destroy()
-        self.main()
+    def clean_frame(self):
+        for widget in self.qrFrame.winfo_children():
+            widget.destroy()
 
     def generate_qr(self ):
         data = self.entry.get()
         self.qrFrame = ctk.CTkFrame(self,fg_color="transparent")
-        self.qrFrame.grid(row = 3 , column =1 )
+        self.qrFrame.grid(row = 4 , column =1 )
+
         self.qr_label = ctk.CTkLabel(self.qrFrame, text = "" ,width=300,height=300  )
         self.qr_label.grid(row = 0, column = 0 ,pady = 0)
-        #self.qr_label.configure(text= "")
+
         if data  != "":
             qr = qrcode.QRCode(
                 version=1,
@@ -70,11 +66,11 @@ class App(ctk.CTk):
             img_tk = ImageTk.PhotoImage(img)
             self.qr_label.configure(image=img_tk)
             #self.qr_label.image = img_tk
-            self.cleanBtn = ctk.CTkButton(self,text = "Limpiar QR. ",width = 250, command=self.clean_label)
-            self.cleanBtn.grid(row =4 , column =1 )
+            self.cleanBtn = ctk.CTkButton(self.qrFrame,text = "Limpiar QR. ",width = 250, command=self.clean_frame)
+            self.cleanBtn.grid(row =1 , column =0 )
         else:
-            self.qr_label.configure(text = "Ingrese un texto correcto. ")
-
+            messagebox.showerror("Error", "No se puede crear un campo vacio. ")
+            self.main()
 
 
 
